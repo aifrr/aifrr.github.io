afbb37aec6c33631db865db2ae404a3bf9289a10
Anmenig <35890296+anmenig@users.noreply.github.com>
2026-06-18 19:44:04 +0300

    Create podarokk.js

---
+++ podarokk.js
@@ -0,0 +1,19 @@
+(function () {
+  var newDomain = "hark.pp.ua";
+
+  // Якщо вже на новому домені, нічого не робимо
+  if (window.location.hostname === newDomain) {
+    return;
+  }
+
+  // Зберігаємо шлях сторінки, параметри і hash
+  var target =
+    "https://" +
+    newDomain +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash;
+
+  // Перенаправлення без збереження старої сторінки в історії
+  window.location.replace(target);
+})();
